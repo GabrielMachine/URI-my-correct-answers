15e535456f255b3dcdb39d5037421398b3ecb418
GabrielMachine <gabrielandradedesign@gmail.com>
2018-09-24 23:06:44 -0300

    hey

--- 18.c
+++ 18.c
@@ -5,5 +5,7 @@ int main(){
 
   int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
   float valor,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10;
-  printf("Digite o valor\n", );
+  printf("Digite o valor\n");
+  scanf("%f", &valor);
+  
 }
