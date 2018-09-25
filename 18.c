a88175c5e3067b50aba260eb367d26cbe754b01c
GabrielMachine <gabrielandradedesign@gmail.com>
2018-09-24 23:17:14 -0300

    new

--- 18.c
+++ 18.c
@@ -7,5 +7,21 @@ int main(){
   float valor,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10;
   printf("Digite o valor\n");
   scanf("%f", &valor);
-  
+  if(valor>=0 && valor<=1000000){
+
+    n1=valor/100;
+    r1=fmod(n1,100);
+
+
+
+
+
+
+
+    //printing
+
+    printf("NOTAS:\n");
+    printf("%d nota(s) de R$ 100.00 \n", n1 );
+  }
+
 }
