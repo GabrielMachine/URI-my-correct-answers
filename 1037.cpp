623fd6c1786c83ca1b333647b7a56cdc2c003d85
Gabriel Andrade <gabrielandradedesign@gmail.com>
2020-06-09 20:52:52 -0300

    Create 1037.cpp

---
+++ 1037.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    
+    float a;
+    std::cin >> a;
+    if(a < 0 || a > 100){
+        std::cout << "Fora de intervalo" << "\n"; 
+    }
+    else if (a >= 0 && a <= 25){
+        std::cout << "Intervalo [0,25]" << "\n";
+    }
+    else if (a > 25 && a <= 50){
+        std::cout << "Intervalo (25,50]" << "\n";
+    }
+    else if (a > 50 && a <= 75){
+        std::cout << "Intervalo (50,75]" << "\n";
+    }
+    else if (a > 75 && a <= 100){
+        std::cout << "Intervalo (75,100]" << "\n";
+    }
+    return 0;
+}
